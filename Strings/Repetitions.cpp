86c1d0c2887362e894fd995fc5ae2d57e173979c
AyushAgrawal18 <agrawalayush638@gmail.com>
2026-07-30 14:08:18 +0530

    Solved 3 CSES questions

---
+++ Strings/Repetitions.cpp
@@ -0,0 +1,62 @@
+// Question Link: https://cses.fi/problemset/task/1069
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* 
+  ****************************************************
+  *                                                  *
+  *             COMPETITIVE PROGRAMMING              *
+  *                                                  *
+  *            Author: Ayush Kumar Agrawal           *
+  *                  Code Smart, Win Big             *
+  *                                                  *
+  ****************************************************
+*/
+
+#define fastio() ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ll long long
+#define pb push_back
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define sz(v) ((int)(v).size())
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define repr(i, a, b) for (int i = a; i >= b; --i)
+#define loop for(int i = 0; i < n; i++)
+#define rloop for(int i = n-1; i >= 0; i--)
+#define yes() cout << "YES\n"
+#define no() cout << "NO\n"
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef vector<pii> vpii;
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+const double PI = acos(-1);
+
+inline void solve() {
+    // Your solution goes here
+    string s;
+    cin>>s;
+    char a=s[0];
+    int count=1;
+    int ans=0;
+    for(int i=1;i<s.length();i++){
+        if(s[i]==a) count++;
+        else{
+            ans=max(ans,count);
+            count=1;
+            a=s[i];
+        }
+    }
+    ans=max(ans,count);
+    cout<<ans<<endl;
+}
+
+int main() {
+    fastio();
+    solve();
+    return 0;
+}
